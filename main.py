738ba21dcc874ba39cae7a389be4520f002e0634
Ilyas2503 <83136147+Ilyas2503@users.noreply.github.com>
2022-03-24 11:02:57 +0600

    def_lesson2

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ def substract(c, d):
     return c - d
 print(substract(100, 50))
 
+def multiply(e, f):
+    return e * f
+print(multiply(5, 5))
+
+def divide(j,h):
+    return j / h
+print(divide(20, 10))
 
